becdd8830348bb461ac46b6691e9d5b638d25e58
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-11-17 19:59:27 -0300

    Adição de atributos em Imovel e Usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -5,15 +5,42 @@ import java.util.List;
 
 public class Usuario {
 
+	private String nome, cpf, email; // vamos usar somente esses atributos para o cadastro para ser mais rapido
 	// ele pode ser alguem interessado ou locador -- FUTURAMENTE REFATORAR E IMPLEMENTAR OBSERVER
 	private List<Aluguel> imoveisAlugados;
 	private List<Anuncio> imoveisAnunciados;
 	
-	public Usuario() {
+	public Usuario(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
 		this.imoveisAlugados = new ArrayList<Aluguel>();
 		this.imoveisAnunciados = new ArrayList<Anuncio>();
 	}
 	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public List<Aluguel> getImoveisAlugados() {
 		return imoveisAlugados;
 	}
