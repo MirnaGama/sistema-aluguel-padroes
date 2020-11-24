d190f50e353f5035e73ac611976b522ceb4fafb8
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-11-23 22:55:39 -0300

    Listar alugueis formatado

--- src/repositorio/AluguelRepositorio.java
+++ src/repositorio/AluguelRepositorio.java
@@ -3,6 +3,7 @@ package repositorio;
 import entidades.Aluguel;
 import entidades.Usuario;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class AluguelRepositorio {
@@ -17,13 +18,16 @@ public class AluguelRepositorio {
     public void listarAlugueis(Usuario usuario) {
         System.out.println("-------------ALUGUEIS----------------");
         if (usuario.getLocacoes().isEmpty()) {
-            System.out.println("Não existe veículos alugados para este usuário ");
+            System.out.println("Nao existe veiculo alugado para este usuario ");
         } else {
+        	SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        	
             for (Aluguel aluguel : usuario.getLocacoes()) {
-                System.out.println(" Veículo: " + " " + aluguel.getVeiculo().getModelo() + " " + aluguel.getVeiculo().getAno() + " " + aluguel.getVeiculo().getCor());
-                System.out.println("Data de locação: " + aluguel.getDataAluguel());
-                System.out.println("Data de entrega: " + aluguel.getValidade());
-                System.out.println("Valor: R$ " + aluguel.getPreco());
+                System.out.println("Veiculo: " + " " + aluguel.getVeiculo().getModelo() + " " + aluguel.getVeiculo().getAno() + " " + aluguel.getVeiculo().getCor());
+                System.out.println("Data de locacao: " + formatter.format(aluguel.getDataAluguel()));
+                System.out.println("Data de entrega: " + formatter.format(aluguel.getValidade()));
+                System.out.println("Tipo de veiculo: "+aluguel.getVeiculo().getTipo());
+                System.out.println("Valor: R$ " + aluguel.getPreco()+"\n");
             }
         }
     }
